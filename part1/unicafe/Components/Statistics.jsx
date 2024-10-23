911e3ab82636dc045be6b47cef8c206832b865f8
frtavonatti <frtavonatti@gmail.com>
2024-10-23 20:33:26 +0100

    Part1d: Unicafe terminado

--- part1/unicafe/Components/Statistics.jsx
+++ part1/unicafe/Components/Statistics.jsx
@@ -1,17 +1,19 @@
 const Statistics = ({ good, neutral, bad }) => {
     const total = good + neutral + bad
 
+    if (total === 0) {
+        return <p>No feedback given</p>
+    }
+
     return (
-      <>
-        <h2>Statistics</h2>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
-        <h3>TOTAL: {total}</h3>
-
-
-      </>
-    )
+        <>
+            <h2>Statistics</h2>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <h3>TOTAL: {total}</h3>
+        </>
+      )
   }
 
   export default Statistics
