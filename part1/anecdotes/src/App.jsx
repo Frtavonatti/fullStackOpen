be35cff22b0c08c23207a38ff140874efcccecef
frtavonatti <frtavonatti@gmail.com>
2024-10-23 20:36:58 +0100

    Part1d: Iniciando ejercicio anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Anecdotes</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
