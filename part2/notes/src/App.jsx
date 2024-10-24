d41ab5692c2e0be303c16ef8045a2b00a4f14b9c
frtavonatti <frtavonatti@gmail.com>
2024-10-24 19:33:15 +0100

    Part2a en proceso: Iniciando Ejercicios 2.1 a 2.5

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import Note from './components/Note'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App({ notes }) {
-  // const [count, setCount] = useState(0)
+  // console.log(notes)
 
   return (
     <>
@@ -12,11 +13,12 @@ function App({ notes }) {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <h1>Note App</h1>
+        
         <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
+          {notes.map(note => 
+            <Note key={note.id} note={note} />
+          )}
+        </ul>
       </div>
     </>
   )
