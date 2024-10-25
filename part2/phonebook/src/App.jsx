2a31d05786a6ef3acfe2192be2974b78ef3c8f43
frtavonatti <frtavonatti@gmail.com>
2024-10-26 00:38:50 +0100

    **Transicion de computador de desarrollo**

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
   return (
     <>
       <div>
@@ -11,8 +16,23 @@ function App() {
           <h1>Phonebook App</h1>  
         </a>
       </div>
+      
+      <div>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      ...
+    </div>
+
     </>
   )
-}
+} 
 
 export default App
