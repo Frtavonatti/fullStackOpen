ed9a4394fef366f1ca73db4888952d92f3c2f2ee
frtavonatti <frtavonatti@gmail.com>
2024-10-25 13:20:29 +0100

    Part2b iniciando ejercicios

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+          <h1>Phonebook App</h1>  
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
