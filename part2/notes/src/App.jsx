b5eb48bc28a7a960b176b64f40e4ec0c2c9c40ef
frtavonatti <frtavonatti@gmail.com>
2024-10-25 12:40:28 +0100

    Part2b en proceso: trabajando en notesApp

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -3,8 +3,21 @@ import Note from './components/Note'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App({ notes }) {
-  // console.log(notes)
+function App(props) {
+  const [notes, setNotes] = useState(props.notes)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const query = event.target[0].value
+    const newNote = { 
+        id: notes.length + 1, 
+        content: query, 
+        important: false 
+      } 
+    setNotes(notes.concat(newNote))
+  }
+  
+  console.log(notes);
 
   return (
     <>
@@ -13,13 +26,18 @@ function App({ notes }) {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
         <h1>Note App</h1>
-        
-        <ul>
-          {notes.map(note => 
-            <Note key={note.id} note={note} />
-          )}
-        </ul>
       </div>
+        
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input/>
+        <button type="submit">save</button>
+      </form>   
     </>
   )
 }
