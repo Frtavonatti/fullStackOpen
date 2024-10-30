2cbd23f37686615d1fdbab9b6607867a438bb141
frtavonatti <frtavonatti@gmail.com>
2024-10-30 14:30:19 +0000

    *** Updating partc exercises***

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Search from '../components/Search'
 import Input from '../components/Input'
 import Numbers from '../components/Numbers'
@@ -6,21 +7,20 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
 
-
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get('http://localhost:3001/persons')
+      setPersons(res.data)
+    }
+    fetchData()
+  }, [])
 
   const filteredPersons = persons.filter(person => person.name.toLocaleLowerCase().includes(newSearch))
-  
 
   return (
     <>
