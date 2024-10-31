bc8880da74f737d29dd097b3bbd5555e8198c674
Frtavonatti <frtavonatti@gmail.com>
2024-10-31 22:41:14 +0000

    Part2e: Iniciando ejercicios

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+        <h1>Countries List</h1>
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
