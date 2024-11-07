8677d7a3cf54b37a89227ba7e53eed78d396ccc8
Frtavonatti <frtavonatti@gmail.com>
2024-11-07 14:18:00 +0000

    Part3c en proceso: iniciando ejercicios 3.15-3.18

--- part3/playground/index.js
+++ part3/playground/index.js
@@ -1,94 +1,116 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.use(express.static("dist"));
 
 // MODELS
-const Note = require('./models/note')
+const Note = require("./models/note");
 
 // MIDDLEWARE
-// Request logger 
+// Request logger
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(express.json()) //json-parser: nos permite acceder a datos enviados en body de un request (POST, PUT, PATCH)
-app.use(cors())
-app.use(requestLogger)
-app.use(express.static('dist'))
+const cors = require("cors");
 
-// Unknown endpoint 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-
+app.use(cors());
+app.use(express.json()); //json-parser: nos permite acceder a datos enviados en body de un request (POST, PUT, PATCH)
+app.use(requestLogger);
 
 // ROUTES
-// GET
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes =>  {
-        response.json(notes)
+// ###GET
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
+
+app.get("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findById(id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
     })
-})
+    .catch((error) => next(error));
+});
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    Note.findById(id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.error(error)
-            response.status(500).send({ error: 'malformatted id' })
-        })
-})
-
-
-// DELETE
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    Note.findByIdAndDelete(id)
-        .then(deletedNote => {
-            response.json(deletedNote)
-        })
-        // TO-DO: catch
-})
-
-
-// POST 
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({ 
-            error: 'content missing' 
-        })
-    }
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
+// ###DELETE
+app.delete("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  Note.findByIdAndDelete(id)
+    .then((deletedNote) => {
+      response.json(deletedNote);
     })
+    .catch((error) => next(error));
+});
 
-    note.save().then(savedNote => {
-        response.json(savedNote)
+// ###POST
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
+// ###PUT
+app.put("/api/notes/:id", (request, response, next) => {
+  const id = request.params.id;
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  // TO-DO: entender el terncer parametro
+  Note.findByIdAndUpdate(id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
     })
-})
+    .catch((error) => next(error));
+});
 
+// MIDDLEWARE
+// ###UNKNOWN ENDPOINT
+app.use((request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+});
 
-// UNKNOWN ENDPOINT
-app.use(unknownEndpoint)
+// ###ERROR HANDLlER
+app.use((error, request, response, next) => {
+  console.error(error);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+});
 
 // PORT CONECTION
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
