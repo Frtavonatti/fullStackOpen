3a4cf7ac70f088698e56c55ba20cc6cedb9734ee
Frtavonatti <frtavonatti@gmail.com>
2024-11-09 14:37:37 +0000

    Part4a en proceso: ejercicio 4.1 terminado

--- part4/noteApp/app.js
+++ part4/noteApp/app.js
@@ -1,3 +1,5 @@
+// CONFIGURACION
+// 1. Configuración y dependencias
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
@@ -7,8 +9,8 @@ const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+// 2. Configuración de Mongoose (se debe conectar a la BD antes de comenzar a manejar solicitudes)
 mongoose.set('strictQuery', false)
-
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
@@ -19,13 +21,16 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+// MIDDLEWARE global
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+// RUTAS
 app.use('/api/notes', notesRouter)
 
+//MIDDLEWARE manejo de rutas (deben ir despues de las rutas)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
