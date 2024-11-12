6f5d0cb2f5d6a8673708f0be7a0789951479fd9a
Frtavonatti <frtavonatti@gmail.com>
2024-11-12 16:26:12 +0000

    Part4b en proceso: Ejercicio 4.12 terminado

--- part4/blogList/test/blog_api.test.js
+++ part4/blogList/test/blog_api.test.js
@@ -7,7 +7,6 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const { log } = require('node:console')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -35,12 +34,12 @@ test('GET request returns the correct ammount of blogs in the array', async () =
 test('unique identifier is `id` and not `_id`', async () => {
     const response = await api.get('/api/blogs')
     const blog = response.body[0]
-    // console.log(Object.keys(blog))
+
     assert(blog.hasOwnProperty('id') && !blog.hasOwnProperty('_id'))
 })
 
-// //4.10
-test.only('POST requests create a new object in blogsArray', async () => {
+//4.10
+test('POST requests create a new object in blogsArray', async () => {
     const newBlog = {
         id: "111",
         title: "Testing POST requests",
@@ -55,19 +54,47 @@ test.only('POST requests create a new object in blogsArray', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-    const notesAfterPOST = await api.get('/api/blogs')
-    assert.strictEqual(notesAfterPOST.body.length, helper.blogs.length + 1)
+    const blogsAfterPOST = await api.get('/api/blogs')
+    assert.strictEqual(blogsAfterPOST.body.length, helper.blogs.length + 1)
 })
 
-// //4.11
-// test('if there is no `likes` prop, it will have the value 0 by default',async () => {
+//4.11
+test('if there is no `likes` prop, it will have the value 0 by default',async () => {
+    const newBlog = {
+        id: "111",
+        title: "Testing POST requests",
+        author: "Me",
+        url: "post.com"
+    }
 
-// })
+    const lastBlogPost = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
 
-// //4.12
-// test('if there is no `title` or `` props, the response will be 400 Bad Request', () => {
+    const response = lastBlogPost.body
+    assert.strictEqual(response.likes, 0)        
+    console.log('Likes: ', response.likes);
+})
 
-// })
+//4.12
+test.only('if there is no `title` or `URL` props, the response will be 400 Bad Request', async () => {
+    const newBlog = {
+        id: "111",
+        title: "Testing POST requests",
+        author: "Me",
+    }
+
+    const lastBlogPost = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+    .expect('Content-Type', /application\/json/)
+
+    console.log('res.status: ', lastBlogPost.status)
+    assert.strictEqual(lastBlogPost.status, 400)
+})
 
 after(async () => {
     await mongoose.connection.close()
--- part4/blogList/toDo.md
+++ part4/blogList/toDo.md
@@ -15,15 +15,21 @@
 - [x] Refactoriza la operación para usar async/await en lugar de promesas.
 
 ### 4.11*: Pruebas de Lista de Blogs, paso 4
-- [ ] Escribe una prueba que verifique que si la propiedad `likes` falta en la solicitud, tendrá el valor 0 por defecto.
-- [ ] Realiza los cambios necesarios en el código para que pase la prueba.
+- [x] Escribe una prueba que verifique que si la propiedad `likes` falta en la solicitud, tendrá el valor 0 por defecto.
+- [x] Realiza los cambios necesarios en el código para que pase la prueba.
 
 ### 4.12*: Pruebas de Lista de Blogs, paso 5
 - [ ] Escribe una prueba que verifique que si faltan las propiedades `title` o `url` en los datos solicitados, el backend responde con el código de estado 400 Bad Request.
 - [ ] Realiza los cambios necesarios en el código para que pase la prueba.
 
+### 4.13: Expansiones de la Lista de Blogs, paso 1
+- [ ] Implementa la funcionalidad para eliminar un solo recurso de publicación de blog usando async/await.
+- [ ] Implementa pruebas para esta funcionalidad.
 
-
+### 4.14: Expansiones de Listas de Blogs, paso 2
+- [ ] Implementa la funcionalidad para actualizar la información de una publicación de blog individual usando async/await.
+- [ ] La aplicación debe actualizar la cantidad de likes para una publicación de blog.
+- [ ] Implementa pruebas para esta funcionalidad.
 
 # Part4a
 ## Ejercicios 4.3.-4.7.
