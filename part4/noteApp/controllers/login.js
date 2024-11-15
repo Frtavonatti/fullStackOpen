400455d6f3c8fd767b08c0488a29d0578c17f0c4
Frtavonatti <frtavonatti@gmail.com>
2024-11-15 12:32:23 +0000

    Part4d en proceso: autenticación basada en token

---
+++ part4/noteApp/controllers/login.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+  // Validación de recibir estas variables antes de la comparacion
+  // if (!user || !user.passwordHash) {
+  //   console.log(user)
+  //   return response.status(401).json({
+  //     error: 'invalid username or password',
+  //     type: 'when requesting'
+  //   })
+  // } else {
+  //   console.log(`User: ${user} --- HASH: ${user.passwordHash}`)
+  // }
+
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
