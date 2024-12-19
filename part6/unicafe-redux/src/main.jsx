9d6f54d1895e4e6901e7be0ab452ab394ec2725c
Frtavonatti <frtavonatti@gmail.com>
2024-12-19 10:09:18 +0000

    Part6a en proceso: Ejercicios 6.1-6.2 terminados

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
 
 import { createStore } from 'redux'
 import reducer from './reducer'
@@ -7,21 +8,25 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
+  const handleClick = (type) => () => {
+    store.dispatch({ type })
   }
+
+  const good = handleClick('GOOD')
+  const ok = handleClick('OK')
+  const bad = handleClick('BAD')
+  const reset = handleClick('ZERO')
 
   return (
     <div>
       <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <button onClick={ok}>ok</button> 
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset</button>
+
+      <div>good: {store.getState().good}</div>
+      <div>ok: {store.getState().ok}</div>
+      <div>bad: {store.getState().bad}</div>
     </div>
   )
 }
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -1,6 +1,8 @@
 import deepFreeze from 'deep-freeze'
 import counterReducer from './reducer'
 
+import { beforeEach, describe, test, expect } from '@jest/globals'
+
 describe('unicafe reducer', () => {
   const initialState = {
     good: 0,
@@ -18,18 +20,40 @@ describe('unicafe reducer', () => {
     expect(newState).toEqual(initialState)
   })
 
-  test('good is incremented', () => {
-    const action = {
-      type: 'GOOD'
-    }
-    const state = initialState
-
-    deepFreeze(state)
-    const newState = counterReducer(state, action)
-    expect(newState).toEqual({
-      good: 1,
-      ok: 0,
-      bad: 0
+  describe('when one button is clicked', () => {
+    let newState;
+    beforeEach(() => {
+      const action = {
+        type: 'GOOD'
+      }
+      const state = initialState
+      deepFreeze(state)
+      newState = counterReducer(state, action)
+    });
+    
+    test('good is incremented', () => {
+      expect(newState).toEqual({
+        good: 1,
+        ok: 0,
+        bad: 0
+      })
     })
+
+    test('reset sets every state to zero', () => {
+      const action = {
+        type: 'ZERO'
+      }
+
+      const state = newState
+  
+      deepFreeze(state)
+      const resetState = counterReducer(state, action)
+      expect(resetState).toEqual({
+        good: 0,
+        ok: 0,
+        bad: 0
+      })
+    })
+
   })
 })
