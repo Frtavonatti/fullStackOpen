a9d4f2a6f21d28194ef5c31f880c7c18681bdcc6
Frtavonatti <frtavonatti@gmail.com>
2024-12-22 18:23:44 +0000

    Part6a en proceso: iniciando ejercicios 6.10 - 6.13

--- part6/noteRedux/src/main.jsx
+++ part6/noteRedux/src/main.jsx
@@ -1,28 +1,21 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit' // import { createStore, combineReducers } from 'redux'
 
 import App from './App'
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
-// import { createNote } from './reducers/noteReducer'
-// import { filterChange } from './reducers/filterReducer'
 import './index.css'
 
-// El reducer combinado funciona de tal manera que cada acción es controlada en cada parte del reducer combinado, 
-// o en otras palabras, cada reducer "escucha" a todas las acciones despachadas
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
 
-const store = createStore(reducer)
 console.log(store.getState())
-
-// store.subscribe(() => console.log(store.getState()))
-// store.dispatch(filterChange('IMPORTANT'))
-// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/noteRedux/src/reducers/filterReducer.js
+++ part6/noteRedux/src/reducers/filterReducer.js
@@ -1,19 +1,32 @@
-const filterReducer = (state = 'ALL', action) => {
-  // console.log('ACTION: ', action)
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = 'ALL'
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
+
+export const { filterChange } = filterSlice.actions 
+export default filterSlice.reducer
+
+// const filterReducer = (state = 'ALL', action) => {
+//   switch (action.type) {
+//     case 'SET_FILTER':
+//       return action.payload
+//     default:
+//       return state
+//   }
+// }
 
 // Actions
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+// export const filterChange = filter => {
+//   return {
+//     type: 'SET_FILTER',
+//     payload: filter,
+//   }
+// }
