37e5be30797a2345d7737af5e9e693483bb37d6b
Frtavonatti <frtavonatti@gmail.com>
2024-12-24 14:04:05 +0000

    Part6c en proceso: Ejercicio 6.16 terminado - Inicialización del store con action creator async (redux thunk)

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,19 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
 import './App.css'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    anecdoteService.getAll()
-      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, []);
 
   return (
