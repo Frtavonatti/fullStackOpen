0d1a9bfe673798b01633b1d5f092f960a79eb19f
frtavonatti <frtavonatti@gmail.com>
2024-12-29 12:07:38 +0000

    Part7a: Finished exercise 7.2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,
    Routes, Route, Link 
   } from 'react-router-dom'
 import AnecdoteList from './components/AnecdoteList'
+import Anecdote from './components/Anecdote'
 import CreateNew from './components/CreateNew'
 import About from './components/About'
 import Footer from './components/Footer'
@@ -62,6 +63,7 @@ const App = () => {
       </div>
 
       <Routes>
+        <Route path='/anecdotes/:id' element={<Anecdote anecdotes={anecdotes}/>} />
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes}/>} />
         <Route path='/create' element={<CreateNew addNew={addNew}/>} />
         <Route path='/about' element={<About/>} />
