f3097604af979ad3c223b2e442a5b146a7046b17
frtavonatti <frtavonatti@gmail.com>
2025-01-04 20:58:42 +0000

    Part9b : Doing exercise 9.3 -  Add argument parsing and error handling to BMI calculator

--- part9/bmiCalculator/bmiCalculator.ts
+++ part9/bmiCalculator/bmiCalculator.ts
@@ -1,21 +1,44 @@
-// BMI: mass / height^2
-
-const calculateBmi = (height: number, mass: number): string => {
-  const bmi: number = mass / (height**2) * 10000;
-
-  if (bmi < 18.5) {
-    return 'Underweight';
-  } else if (bmi < 24.9) {
-    return 'Normal (healthy weight)';
-  } else if (bmi < 29.9) {
-    return 'Overweight';
-  } else if (bmi < 34.9) {
-    return 'Obese Class I (Moderate)';
-  } else if (bmi < 39.9) {
-    return 'Obese Class II (Severe)';
+const parseArguments = () => {
+  if (process.argv.length < 4) throw new Error("Not enough arguments");
+  if (process.argv.length > 4) throw new Error("Too many arguments");
+  
+  if (!isNaN(Number(process.argv[2])) && !isNaN(Number(process.argv[3]))) {
+    return {
+      height: Number(process.argv[2]),
+      mass: Number(process.argv[3])
+    }
   } else {
-    return 'Obese Class III (Very severe)';
+    throw new Error("Height and mass must be numbers");
   }
 }
 
-console.log(calculateBmi(156, 60))
+const calculateBmi = (height: number, mass: number): string => { 
+  const bmi: number = mass / (height**2) * 10000; 
+  let result: string;
+  
+  if (bmi < 18.5) {
+    result = 'Underweight';
+  } else if (bmi < 24.9) {
+    result = 'Normal (healthy weight)';
+  } else if (bmi < 29.9) {
+    result = 'Overweight';
+  } else if (bmi < 34.9) {
+    result = 'Obese Class I (Moderate)';
+  } else if (bmi < 39.9) {
+    result = 'Obese Class II (Severe)';
+  } else {
+    result = 'Obese Class III (Very severe)';
+  }
+  return result;
+}
+
+try {
+  const { height, mass } = parseArguments();
+  console.log(calculateBmi(height, mass));
+} catch (e) {
+  let errorMessage = 'An error occurred: ';
+  if (e instanceof Error) {
+    errorMessage = errorMessage.concat(e.message);
+  }
+  console.log(errorMessage);
+}
