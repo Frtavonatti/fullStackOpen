a08f5ae5658937baa7b8b05916a589812dd3155c
frtavonatti <frtavonatti@gmail.com>
2025-01-20 11:13:35 +0000

    - Part9d: Finished exercise 9.16 Add Tailwind CSS setup, Axios integration, and diary entry types; update configurations and styles

--- part9/ilari/client/src/App.tsx
+++ part9/ilari/client/src/App.tsx
@@ -1,16 +1,50 @@
-import reactLogo from './assets/react.svg'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { NonSensitiveDiaryEntry } from "./types";
+import reactLogo from "./assets/react.svg";
 
 function App() {
+  const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/diaries")
+      .then((response) => {
+        setDiaries(response.data);
+        setError(null);
+      })
+      .catch((error) => {
+        console.error("Error fetching diaries:", error);
+        setError("error fetching diaries" + error.message);
+      });
+  }, []);
+
   return (
-    <>
-      <div>
+    <div className="rounded-lg bg-zinc-800 px-12 py-6 shadow-lg">
+      <div className="mb-8 flex justify-center">
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo h-20 w-20" alt="React logo" />
         </a>
       </div>
-      <h1>Ilari Flights</h1>
-    </>
-  )
+      <h1 className="mb-8">Ilari Flights</h1>
+      {error && <p>{error}</p>}
+      <section>
+        {diaries.map((diary) => (
+          <div
+            key={diary.id}
+            className="mb-4 rounded-lg border border-solid border-white p-2 shadow-lg"
+          >
+            <h3 className="mb-2 text-xl font-bold">{diary.date}</h3>
+            <ul>
+              <li>Visibility: {diary.visibility}</li>
+              <li>Weather: {diary.weather} </li>
+            </ul>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- part9/ilari/server/src/index.ts
+++ part9/ilari/server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import diaryRouter from './routes/diaries';
 import { logger } from './utils/middleware';
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
