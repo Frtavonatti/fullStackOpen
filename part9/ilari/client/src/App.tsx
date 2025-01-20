684455ba513cef0759482101e882300271302795
frtavonatti <frtavonatti@gmail.com>
2025-01-20 10:02:53 +0000

    Part9d: Starting exercises 9.16-9.19 - Set up Vite and TypeScript for client; implement initial React app structure

---
+++ part9/ilari/client/src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Ilari Flights</h1>
+    </>
+  )
+}
+
+export default App
