44a1bc420264bb7cd06b699101a31f205c53348e
frtavonatti <frtavonatti@gmail.com>
2025-01-21 16:18:26 +0000

    Part9e: Finished exercise 9.22 - Refactor patient data structure and update types for better clarity and type safety

--- part9/patientor/server/src/types.ts
+++ part9/patientor/server/src/types.ts
@@ -1,17 +1,67 @@
-export interface Diagnoses {
+// Diagnosis
+export interface Diagnosis {
   code: string;
   name: string;
   latin?: string;
 }
 
+//Entries
+// export enum EntryType {
+//   Hospital = 'Hospital',
+//   HealthCheck = 'HealthCheck',
+//   Occupational = 'OccupationalHealthcare'
+// }
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface BaseEntry {
+  id: string,
+  date: string,
+  specialist: string,
+  description: string,
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  // type: EntryType.HealthCheck;
+  type: 'HealthCheck'
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  // type: EntryType.Hospital,
+  type: 'Hospital',
+  discharge?: {
+    date: string;
+    criteria: string;
+  },
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  // type: EntryType.Occupational,
+  type: 'OccupationalHealthcare',
+  employerName: string,
+  sickLeave?: {
+    startDate: string,
+    endDate: string
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthCareEntry
+  | HealthCheckEntry;
+
+// Patients
 export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other' 
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
 }
 
 export interface Patients {
@@ -21,7 +71,7 @@ export interface Patients {
   ssn: string,
   gender: Gender,
   occupation: string,
-  entries: Entry
+  entries: Entry[]  
 }
 
 export type NonSensitivePatiensData =  Omit<Patients, 'ssn' | 'entries'>
