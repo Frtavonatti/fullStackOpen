95e4154d5d194669aa0e36b54c64b406a8153c2f
frtavonatti <frtavonatti@gmail.com>
2025-01-22 18:16:34 +0000

    Enhance EntryDetails component: add entry type icons and improve type annotations

--- part9/patientor/client/src/components/PatientView/EntryDetails.tsx
+++ part9/patientor/client/src/components/PatientView/EntryDetails.tsx
@@ -1,11 +1,25 @@
 import { Typography, Button } from '@mui/material';
+import { MonitorHeart, LocalHospital, Work } from '@mui/icons-material';
 import DiagnosesCodes from './DiagnosesCodes';
-import { Diagnosis, Patient } from '../../types';
+import { Diagnosis, Patient, Entry } from '../../types';
 
 interface Props {
   patient: Patient,
   diagnosis: Diagnosis[]
 }
+
+const getEntryTypeIcon = (entry: Entry) => {
+  switch(entry.type) {
+    case 'HealthCheck': 
+      return <MonitorHeart/>;
+    case 'Hospital': 
+      return <LocalHospital/>;
+    case 'OccupationalHealthcare':
+      return <Work/>;
+    default:
+      return null; 
+  }
+};
 
 const entryCardStyle = {
   padding: '1rem',
@@ -14,7 +28,7 @@ const entryCardStyle = {
   borderRadius: '5px'
 };
 
-const EntryDetails = ({ patient, diagnosis }: Props) => {
+const EntryDetails: React.FC<Props> = ({ patient, diagnosis }) => {
   return (
     <div style={{marginTop: '4rem'}}>
     { patient.entries.length > 0 
@@ -24,18 +38,16 @@ const EntryDetails = ({ patient, diagnosis }: Props) => {
       <div>
         { patient.entries.map((entry) => (
           <div key={entry.id} style={entryCardStyle}>
-            <Typography> {entry.date} - {entry.type} </Typography>
+            <Typography> {entry.date} - {entry.type} {getEntryTypeIcon(entry)} </Typography>
             { entry.type === 'OccupationalHealthcare' && <Typography> Employer: {entry.employerName} </Typography> }
             <Typography> {entry.description} </Typography>
             <Typography> Diagnosed by: {entry.specialist} </Typography>
             { entry.diagnosisCodes && <DiagnosesCodes diagnosis={diagnosis} codes={entry.diagnosisCodes}/> }
           </div>
-          ))
-        }
+        ))}
       </div>
-
-    <Button variant="contained" color="primary">Add new entry</Button>
-  </div>
+      <Button variant="contained" color="primary">Add new entry</Button>
+    </div>
   );
 };
 
