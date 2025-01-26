78e9627d386af5b0685f0d2d41d3a978b405949f
frtavonatti <frtavonatti@gmail.com>
2025-01-26 13:10:14 +0000

    Part10b: Finished exercise 10.7 - Refactor AppBar component to use ScrollView for horizontal layout

--- part10/rate-repository-app/src/components/AppBar.jsx
+++ part10/rate-repository-app/src/components/AppBar.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, ScrollView, StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme'
 
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: theme.colors.appBar,
-    width: 412,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'start',
-    justifyContent: 'space-evenly',
-    paddingBottom: 30
-  },
-});
-
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab text="Repositories" to="/" />
-      <AppBarTab text="Create a review" to="/review" />
-      <AppBarTab text="Sign in" to="/signin" />
+      <ScrollView horizontal contentContainerStyle={styles.scrollview}>
+        <AppBarTab text="Repositories" to="/" />
+        <AppBarTab text="Create a review" to="/review" />
+        <AppBarTab text="Sign in" to="/signin" />
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: theme.colors.appBar,
+    width: 412,
+  },
+  scrollview: {
+    paddingTop: Constants.statusBarHeight,
+    paddingBottom: 30,
+    display: 'flex',
+    flexDirection: 'row',
+    flexGrow: 1,
+    justifyContent: 'center',
+  }
+});
 
 export default AppBar;
--- part10/rate-repository-app/src/components/AppBarTab.jsx
+++ part10/rate-repository-app/src/components/AppBarTab.jsx
@@ -4,7 +4,7 @@ import Text from './Text';
 const AppBarTab = ({ text, to }) => {
   return (
       <Link to={to}>
-        <Text fontSize="subheading" fontWeight="bold" style={{color: '#fff'}}>
+        <Text fontSize="subheading" fontWeight="bold" style={{color: '#fff', paddingLeft: 14}}>
           {text}
         </Text>
       </Link>
