f201900384b2b6b90371d03682030426142cc44a
frtavonatti <frtavonatti@gmail.com>
2025-01-30 17:27:04 +0000

    Add phonebook search individual person functionality with Apollo Client

--- part08/phonebook/client/src/App.jsx
+++ part08/phonebook/client/src/App.jsx
@@ -1,17 +1,34 @@
+import { gql, useQuery } from '@apollo/client'
+import Persons from './components/Person'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons  {
+      name
+      phone
+      id
+    }
+  }
+`
 
 function App() {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
     <>
       <div>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo" alt="React logo" />
         </a>
       </div>
       <h1>Phonebook</h1>
 
+      <Persons persons={result.data.allPersons}/>
     </>
   )
 }
