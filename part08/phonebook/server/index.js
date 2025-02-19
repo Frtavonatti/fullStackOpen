84cf3707d57df6226e8a07f267c1695e440d1223
frtavonatti <frtavonatti@gmail.com>
2025-02-19 15:04:01 +0000

    Part8e: Refactor Apollo Server setup to use Express middleware and improve structure

--- part08/phonebook/server/index.js
+++ part08/phonebook/server/index.js
@@ -1,7 +1,14 @@
 import { ApolloServer } from '@apollo/server'
-import { startStandaloneServer } from '@apollo/server/standalone'
-import mongoose from 'mongoose'
+import { expressMiddleware } from '@apollo/server/express4'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+// import { WebSocketServer } from 'ws'
+// import useServer from 'graphql-ws/lib/use/ws'
+import express from 'express'
+import cors from 'cors'
+import http from 'http'
 import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
 import 'dotenv/config'
 import typeDefs from './graphql/typeDefs.js'
 import resolvers from './graphql/index.js'
@@ -22,30 +29,68 @@ mongoose.connect(MONGODB_URI)
     console.log('error connection to MongoDB:', error.message)
   })
 
-// Apollo Server Config
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+// Apollo Server Config: setup is now within a function
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  // The object returned by the context is given to all resolvers as their third parameter.
-  context: async ({ req }) => {
-    const auth = req ? req.headers.authorization : null    
-    if (auth && auth.toLowerCase().startsWith('bearer ')) {
-      try {
-        const decodedToken = jwt.verify(
-          auth.substring(7), process.env.JWT_SECRET
-        )
-        const currentUser = await User.findById(decodedToken.id)
-        return { currentUser }
-      } catch (error) {
-        console.error('Token verification failed:', error)
-      }
-    }
-    return {}
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  })
+
+  await server.start()
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SECRET)
+          const currentUser = await User.findById(decodedToken.id).populate(
+            'friends'
+          )
+          return { currentUser }
+        }
+      },
+    }),
+  )
+
+  const PORT = 4000
+
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}`)
+  )
+}
+
+start()
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// })
+
+// startStandaloneServer(server, {
+//   listen: { port: 4000 },
+//   // The object returned by the context is given to all resolvers as their third parameter.
+//   context: async ({ req }) => {
+//     const auth = req ? req.headers.authorization : null    
+//     if (auth && auth.toLowerCase().startsWith('bearer ')) {
+//       try {
+//         const decodedToken = jwt.verify(
+//           auth.substring(7), process.env.JWT_SECRET
+//         )
+//         const currentUser = await User.findById(decodedToken.id)
+//         return { currentUser }
+//       } catch (error) {
+//         console.error('Token verification failed:', error)
+//       }
+//     }
+//     return {}
+//   },
+// }).then(({ url }) => {
+//   console.log(`Server ready at ${url}`)
+// })
