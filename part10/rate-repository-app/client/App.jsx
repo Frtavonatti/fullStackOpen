215c91cef9863ab354aab9c3ac4438914063072e
frtavonatti <frtavonatti@gmail.com>
2025-02-20 17:01:35 +0000

    Add Apollo Client to React Native app
    - Integrate @apollo/client and graphql dependencies
    - Wrap App component with ApolloProvider
    - Import apolloClient utility in App.jsx
    - Update package.json and package-lock.json with new dependencies

--- part10/rate-repository-app/client/App.jsx
+++ part10/rate-repository-app/client/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { NativeRouter } from 'react-router-native';
+import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
+import client from './src/utils/apolloClient';
 
 const App = () => {
   return (
-    <NativeRouter>
-      <Main />
-    </NativeRouter>
+    <ApolloProvider client={client}>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+    </ApolloProvider>
   )
 };
 
