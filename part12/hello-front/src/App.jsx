976cf6c6f47d73acdebc6d07b6a456c6e9635124
frtavonatti <frtavonatti@gmail.com>
2025-10-18 11:38:07 +0100

    Part12c: Starting exercise 12.15

--- part12/hello-front/src/App.jsx
+++ part12/hello-front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello from Docker</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Now local development is connected to the Docker container!
       </p>
     </>
   )
